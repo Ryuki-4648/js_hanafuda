67deb9e3d6e490084a359b95525af07de668bfd0
Ryuki_39 <ryukiyoshi@sidethree.co.jp>
2021-09-01 17:56:44 +0900

    selectbox classのつけ外しに変更

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ $.ajax({
     dataType: "json",
   })
 
-  // ----- 成功した場合の処理 ----- //
+  // ----- 成功した場合 ----- //
   .done(function(data) {
     //console.log(data);
     //console.log(data.items.length);
 
-    // テーブルに出力
+    // ■ 課題1: テーブルに出力
     var tBody = $('#tBody');
     $.each(data.items, function(i, v){
       var tdMonth= '<td>' + v.month + '月' + '</td>';
@@ -19,47 +19,66 @@ $.ajax({
       tBody.append('<tr>' + tdMonth + tdName + tdScore + '</tr>');
     });
 
-    // セレクトボックス
+    // ■ 課題2: セレクトボックス
     $('#select01, #select02').on('change', function(){
       // 選択した値を取得
       var select01 = $('#select01 option:selected').text();
       var value01 = $('#select01').val();
       var select02 = $('#select02 option:selected').text();
       var value02 = $('#select02').val();
-    
-      $.each($('#tBody tr'), function (index, elem) {
+
+      $.each($('#tBody tr'), function () {
         if (value01 == "") {
-          $(elem).css('display', '');
+          $(this).removeClass('hidden');
           return true;
         }
         if (value02 == "") {
-          $(elem).css('display', '');
+          $(this).removeClass('hidden');
           return true;
         }
         
-        var row_text = $(elem).text();
-        if (row_text.indexOf(select01) != -1) { // 月 一致した場合
-          if (row_text.indexOf(select02) != -1) { // 点 一致した場合
-            $(elem).css('display', '');
+        var text = $(this).text();
+        if (text.indexOf(select01) != -1) {
+          if (text.indexOf(select02) != -1) {
+            $(this).removeClass('hidden');
           } else {
-            $(elem).css('display', 'none');
-          }
-        } else if (row_text.indexOf(select02) != -1) { // 点 一致した場合
-          if (row_text.indexOf(select01) != -1) { // 月 一致した場合
-            $(elem).css('display', '');
-          } else {
-            $(elem).css('display', 'none');
+            $(this).addClass('hidden');
           }
         } else {
-          $(elem).css('display', 'none');
+          if (text.indexOf(select02) != -1) {
+            $(this).addClass('hidden');
+          } else {
+            $(this).addClass('hidden');
+            console.log('hoge');
+          }
         }
       });
     });
 
+    /*
+    $('#select02').on('change', function(){
+      var select02 = $('#select02 option:selected').text();
+      var value02 = $('#select02').val();
+
+      $.each($('#tBody tr'), function () {
+        if (value02 == "") {
+          $(this).removeClass('hidden');
+          return true;
+        }
+
+        var text = $(this).text();
+        if (text.indexOf(select02) != -1) {
+          $(this).removeClass('hidden');
+        } else {
+          $(this).addClass('hidden');
+        }
+      });
+    });
+    */
 
   })
 
-  // ----- 失敗した場合の処理 ----- //
+  // ----- 失敗した場合 ----- //
   .fail(function() {
     alert('取得できませんでした。');
   });
@@ -72,5 +91,5 @@ $.ajax({
   $('#reset').on('click', function(){
     $('[name=select01]').prop("selectedIndex", 0);
     $('[name=select02]').prop("selectedIndex", 0);
-    $('tr').css('display', 'table-row');
+    $('#tBody tr').removeClass('hidden');
   });
